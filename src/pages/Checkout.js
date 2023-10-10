c614188777084d83005522658250e629a8ec09d1
PRANAV696969 <pranavhiremath64@gmail.com>
2023-10-10 03:08:23 -0700

    connected place order api

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import { removeFromCart } from '../redux/actions/cartActions';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,11 +9,38 @@ const Checkout = ({ cart, removeFromCart }) => {
   const navigate = useNavigate();
 
   const handlePlaceOrder = () => {
-    alert('Your order has been placed!');
-    // Clear the cart
-    cart.forEach(book => removeFromCart(book._id));
-    // Redirect to the homepage
-    navigate('/');
+    const orderData = {
+      books: cart.map(book => ({
+        bookID: book._id,
+        title: book.title,
+        authors: book.authors,
+        // Add other book details here
+      })),
+    };
+
+    const token = localStorage.getItem('token');
+
+    console.log('Order Data:', orderData); // Log the order data before making the API call
+
+    axios.post('http://localhost:3001/api/orders', orderData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => {
+        console.log('API Response:', response.data); // Log the API response after receiving it
+        if (response.status === 200) {
+          alert('Your order has been placed!');
+          cart.forEach(book => removeFromCart(book._id));
+          navigate('/');
+        } else {
+          alert('Failed to place the order.');
+        }
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+        alert('There was an error! ' + error);
+      });
   };
 
   return (
@@ -27,7 +55,6 @@ const Checkout = ({ cart, removeFromCart }) => {
               <h3>{book.title}</h3>
               <p>Author: {book.authors}</p>
               <p>Price: $10</p>
-              {/* Display other book details */}
               <button onClick={() => removeFromCart(book._id)}>Remove from Cart</button>
             </div>
           ))}
